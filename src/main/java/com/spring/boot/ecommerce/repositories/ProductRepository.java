cf8f04a523a225f2bea9f5adef653526503d9f05
vitoreribeiro <vitor.e.ribeirosilva@gmail.com>
2026-01-01 12:14:02 -0300

    GET Products by name

--- src/main/java/com/spring/boot/ecommerce/repositories/ProductRepository.java
+++ src/main/java/com/spring/boot/ecommerce/repositories/ProductRepository.java
@@ -1,7 +1,15 @@
 package com.spring.boot.ecommerce.repositories;
 
 import com.spring.boot.ecommerce.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query( "SELECT p                                              \n" +
+            "  FROM Product p                                      \n" +
+            " WHERE UPPER(p.name) LIKE UPPER(CONCAT('%', :name, '%'))" )
+    Page<Product> searchByName(String name, Pageable pageable);
 }
