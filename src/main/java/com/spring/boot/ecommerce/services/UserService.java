cca40c1a0c8c0972d054f5492d4ddfc97aad31e1
vitoreribeiro <vitor.e.ribeirosilva@gmail.com>
2026-02-18 08:42:50 -0300

    Implementando Spring Security

---
+++ src/main/java/com/spring/boot/ecommerce/services/UserService.java
@@ -0,0 +1,38 @@
+package com.spring.boot.ecommerce.services;
+
+import com.spring.boot.ecommerce.entities.Role;
+import com.spring.boot.ecommerce.entities.User;
+import com.spring.boot.ecommerce.projections.UserDetailsProjection;
+import com.spring.boot.ecommerce.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        List<UserDetailsProjection> result = repository.searchUserAndRolesByEmail(username);
+        if (result.isEmpty()) {
+            throw new UsernameNotFoundException("Email not found");
+        }
+
+        User user = new User();
+        user.setEmail(result.get(0).getUsername());
+        user.setPassword(result.get(0).getPassword());
+        for (UserDetailsProjection projection : result) {
+            user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
+        }
+
+        return user;
+    }
+}
