e6f3084f677292659fde78e0d15b0a230a9da7cf
vitoreribeiro <vitor.e.ribeirosilva@gmail.com>
2026-02-18 08:51:00 -0300

    autenticação com JWT

--- src/main/java/com/spring/boot/ecommerce/services/UserService.java
+++ src/main/java/com/spring/boot/ecommerce/services/UserService.java
@@ -5,9 +5,12 @@ import com.spring.boot.ecommerce.entities.User;
 import com.spring.boot.ecommerce.projections.UserDetailsProjection;
 import com.spring.boot.ecommerce.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -34,5 +37,17 @@ public class UserService implements UserDetailsService {
         }
 
         return user;
+    }
+
+    protected User authenticated() {
+        try {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            Jwt jwtPrincipal = (Jwt) authentication.getPrincipal();
+            String username = jwtPrincipal.getClaim("username");
+
+            return repository.findByEmail(username).get();
+        } catch (Exception e) {
+            throw new UsernameNotFoundException("Emailnot found.");
+        }
     }
 }
